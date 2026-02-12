1c4070f50c70abd932a7f2c41163666c740ce75b
olasubomiawodipe <subomiawodipe1@gmail.com>
2026-02-11 18:04:05 -0600

    Add scraper.js structure and implementation plan

---
+++ scripts/scraper.js
@@ -0,0 +1,11 @@
+/*
+ * Detect if an email is open
+ * If so:
+     * Extract the following info:
+         * Sender email address (string)
+         * Links (array of strings)
+         * Subject (string)
+         * Body (string)
+     * Create JSON object
+     * Send to backend API
+ */
